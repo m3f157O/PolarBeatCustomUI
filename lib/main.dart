744b095280b457d834d39545c0c478b51adc2f88
m3f157O <gianmarco.lodari@mail.polimi.it>
2021-10-10 11:27:25 +0200

    Route and provider pattern integration commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 import 'package:auth0/auth0.dart';
 import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -14,15 +15,6 @@ import 'package:provider/provider.dart';
 final FlutterAppAuth appAuth = FlutterAppAuth();
 final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
 
-/// -----------------------------------
-///           Auth0 Variables          
-/// -----------------------------------
-
-const AUTH0_DOMAIN = 'dev-otg2dqth.us.auth0.com';
-const AUTH0_CLIENT_ID = '7fy6wO0sjYrYVKiUstQbecC9FAQ1IJ93';
-
-const AUTH0_REDIRECT_URI = 'com.auth0.flutterdemo://login-callback';
-const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
 
 /// -----------------------------------
 ///           Providers Widget
@@ -40,48 +32,58 @@ class Counter extends ChangeNotifier {
   }
 }
 
+
 /// -----------------------------------
-///           Profile Widget
+///           Auth0 Variables
 /// -----------------------------------
 
-class Profile extends StatelessWidget {
-  final logoutAction;
-  final String name;
-  final String picture;
+const AUTH0_DOMAIN = 'custompolarui.eu.auth0.com';
+const AUTH0_CLIENT_ID = 'X0L4w8enh5WEt3zkKl4U0aZxSCw4hEhG';
 
-  Profile(this.logoutAction, this.name, this.picture);
+const AUTH0_REDIRECT_URI = 'com.auth0.flutterdemo://login-callback';
+const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Named Routes Demo',
+      // Start the app with the "/" named route. In this case, the app starts
+      // on the FirstScreen widget.
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => const FirstScreen(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/second': (context) => const SecondScreen(),
+      },
+    ),
+  );
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          width: 150,
-          height: 150,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.blue, width: 4.0),
-            shape: BoxShape.circle,
-
-          ),
-        ),
-        SizedBox(height: 24.0),
-        Text('Name: $name'),
-        SizedBox(height: 48.0),
-        RaisedButton(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          // Within the `FirstScreen` widget
           onPressed: () {
-            logoutAction();
+            // Navigate to the second screen using a named route.
+            Navigator.pushNamed(context, '/second');
           },
-          child: Text('Logout'),
+          child: const Text('Launch screen'),
         ),
-      ],
+      ),
     );
   }
 }
 
-/// -----------------------------------
-///            Games Widget
-/// -----------------------------------
 
 class Graph extends StatelessWidget
 {
@@ -89,6 +91,7 @@ class Graph extends StatelessWidget
   void _increment(BuildContext context) {
     Provider.of<Counter>(context, listen: false).incrementCounter(context);
   }
+
 
 
   @override
@@ -108,271 +111,34 @@ class Graph extends StatelessWidget
   }
 
 }
-/*
-class Graph extends StatefulWidget {
 
-  @override
-  State<StatefulWidget> createState() {
-    return GraphState();
-  }
-
-
-}
-
-
-class GraphState extends State<Graph> {
-  int rolled=0;
-
-  void _increment() {
-    setState(() {
-      // This call to setState tells the Flutter framework
-      // that something has changed in this State, which
-      // causes it to rerun the build method below so that
-      // the display can reflect the updated values. If you
-      // change _counter without calling setState(), then
-      // the build method won't be called again, and so
-      // nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-
-  List<Widget> getWidget() {
-    var pwdWidgets = <Widget>[];
-
-    if(rolled==0){
-
-      pwdWidgets.add(const Game1());
-
-    }
-    else if(rolled==1)
-    {
-      pwdWidgets.add(const Game2());
-    }
-    else
-    {
-      pwdWidgets.add(Graph());
-    }
-    return pwdWidgets;
-
-  }
-  void roll() {
-    setState(() {
-      // This call to setState tells the Flutter framework
-      // that something has changed in this State, which
-      // causes it to rerun the build method below so that
-      // the display can reflect the updated values. If you
-      // change _counter without calling setState(), then
-      // the build method won't be called again, and so
-      // nothing would appear to happen.
-      if(rolled<2) {
-        rolled++;
-      }
-      else {
-        rolled=0;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called,
-    // for instance, as done by the _increment method above.
-    // The Flutter framework has been optimized to make
-    // rerunning build methods fast, so that you can just
-    // rebuild anything that needs updating rather than
-    // having to individually changes instances of widgets.
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        ElevatedButton(
-          onPressed: _increment,
-          child: const Text('Increment'),
-        ),
-        const SizedBox(width: 16),
-        Text('Count: $_counter'),
-      ],
-    );
-  }
-}
-
-
-class Game1 extends StatelessWidget {
-  const Game1({Key? key}) : super(key: key);
-
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called,
-    // for instance, as done by the _increment method above.
-    // The Flutter framework has been optimized to make
-    // rerunning build methods fast, so that you can just
-    // rebuild anything that needs updating rather than
-    // having to individually changes instances of widgets.
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        ElevatedButton(
-            onPressed: () {  },
-            child: const Text('raatto')
-        ),
-      ],
-    );
-  }
-}
-
-class Game2 extends StatelessWidget {
-  const Game2({Key? key}) : super(key: key);
-
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called,
-    // for instance, as done by the _increment method above.
-    // The Flutter framework has been optimized to make
-    // rerunning build methods fast, so that you can just
-    // rebuild anything that needs updating rather than
-    // having to individually changes instances of widgets.
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        ElevatedButton(
-            onPressed: () {  },
-            child: const Text('raatto')
-        ),
-      ],
-    );
-  }
-}
-*/
-/// -----------------------------------
-///            Login Widget           
-/// -----------------------------------
-class Login extends StatelessWidget {
-  final loginAction;
-  final String loginError;
-
-  const Login(this.loginAction, this.loginError);
-
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called,
-    // for instance, as done by the _increment method above.
-    // The Flutter framework has been optimized to make
-    // rerunning build methods fast, so that you can just
-    // rebuild anything that needs updating rather than
-    // having to individually changes instances of widgets.
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        ElevatedButton(
-            onPressed: loginAction,
-            child: const Text('LOGIN')
-        ),
-      ],
-    );
-  }
-}
-
-
-
-/// -----------------------------------
-///                 App
-/// -----------------------------------
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-/// -----------------------------------
-///              App State            
-/// -----------------------------------
-
-class _MyAppState extends State<MyApp> {
-
-  bool isBusy = false;
-
-  bool isLoggedIn = false;
-
-
-  late String errorMessage='NO GOOD';
-  late String name='RAT2.0';
-  late String picture='BIG';
-
-
-
-  void loginAction()  {
-    setState(() {
-      isLoggedIn=true;
-    });
-
-      print('logged in');
-
-  }
-
-
-  void logoutAction()  {
-    setState(() {
-      isLoggedIn=false;
-    });
-
-    print('logged out');
-
-  }
-
-
-
-  List<Widget> getWidget() {
-    var pwdWidgets = <Widget>[];
-
-    if(isLoggedIn){
-
-      pwdWidgets.add(Profile(logoutAction,name,picture));
-      pwdWidgets.add(Graph());
-
-    }
-    else
-    {
-      pwdWidgets.add(Login(loginAction,errorMessage));
-    }
-    return pwdWidgets;
-
-  }
-
-
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(providers: [
-        ChangeNotifierProvider.value(
+      ChangeNotifierProvider.value(
         value: Counter(),
-        ),
-        ],
+      ),
+    ],
         child : MaterialApp(
-              title: 'Auth0 Demo',
-              home: Scaffold(
-                  appBar: AppBar(
-                      title: Text('Auth0 Demo'),
-                      ),
-                      body: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                children: [
-                                      Column(
-                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                      children: getWidget()
-                                      )
-                                      ]
-                                )
-                              )
-              )
+            title: 'Auth0 Demo',
+            home: Scaffold(
+
+                body: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                           Graph()
+                          ]
+                      )
+                    ]
+                )
+            )
+        )
     );
   }
 
